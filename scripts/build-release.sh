552e93dfa80bd7870c325b61cc6756c97a70e085
CYTMWIA <cytmwia@gmail.com>
2023-10-19 22:19:34 +0800

    copy to release folder

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -5,4 +5,8 @@ rustup target add x86_64-unknown-linux-musl
 
 cargo build --release --target=x86_64-unknown-linux-musl
 
-ls -lh target/*/release/simple-portal
+echo "Outputs:"
+ls -lh target/*/release/simple-portal | awk '{print $5,$(NF)}'
+
+mkdir -p release
+ls target/*/release/simple-portal | awk -F '/' '{printf "%s release/%s-%s",$0,$(NF),$2}' | xargs cp
